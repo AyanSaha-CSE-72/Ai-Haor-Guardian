825c21cd9bdfbc9be177e264d51afff931fdb412
Abdullah-Al-Mahmud <abdullahalmahmudmahmud777@gmail.com>
2026-06-02 13:28:36 +0600

    Fix dark mode flicker on page reload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
     extend: {},
